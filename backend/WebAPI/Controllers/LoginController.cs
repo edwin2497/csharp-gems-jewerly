bf276f71f06c5c2c09afa366931e33ef611cf344
Edwin León <edwin1224leon@outlook.com>
2022-04-27 20:10:49 -0600

    Add and update controllers

--- backend/WebAPI/Controllers/LoginController.cs
+++ backend/WebAPI/Controllers/LoginController.cs
@@ -12,23 +12,48 @@ namespace WebAPI.Controllers
         //[HttpGet("{username}/{password}")]
         //public JsonResult Get(int username, string password)
         [HttpGet()]
-        public JsonResult Get(string password, int username)
+        public JsonResult Get(int username, string password)
         {
             LoginOperations loginOperations = new LoginOperations();
+            //Employee employee = loginOperations.GetEmployeeByUsername(username);
             Employee employee = loginOperations.GetEmployeeByUsername(username);
+            Customer customer = loginOperations.GetCustomerByUsername(username);
 
-            if (employee != null)
+            string type =  loginOperations.GetUserType(username);
+
+
+            if (type.Equals("Employee"))
             {
                 if (password.Equals(employee.Password))
                 {
-                   
                     return new JsonResult(employee);
                 }
-
                 return new JsonResult("Incorrect password");
-                
             }
-            return new JsonResult("User doesn't exist");
+            else if (type.Equals("Customer"))
+            {
+                if (password.Equals(customer.Password))
+                {
+                    return new JsonResult(customer);
+                }
+                return new JsonResult("Incorrect password");
+
+            }
+            return new JsonResult(type);
+
+
+            //    if (employee != null)
+            //    {
+            //        if (password.Equals(employee.Password))
+            //        {
+
+            //            return new JsonResult(employee);
+            //        }
+
+            //        return new JsonResult("Incorrect password");
+
+            //    }
+            //    return new JsonResult("User doesn't exist");
 
 
         }
